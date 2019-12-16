120862662ff4320b5500f6fc4cc559f9256b55cc
solgriprosth <solgriprosth@gmail.com>
2019-12-16 15:08:01 +0100

    Update Result.java

--- src/isprojekt/Result.java
+++ src/isprojekt/Result.java
@@ -3,13 +3,21 @@ package isprojekt;
 public class Result {
 	
 	private String result;
-
-	public String getResult() {
-		return result;
-	}
-
+	private String letterGrade;
+	
 	public void setResult(String result) {
-		this.result = result;
-	} 
-
+		this.result = result; 
+	}
+	
+	public String getResult() {
+		return result; 
+	}
+	
+	public void setLetterGrade(String letterGrade) {
+		this.letterGrade = letterGrade; 
+	}
+	
+	public String getLetterGrade() {
+		return letterGrade;
+	}
 }
