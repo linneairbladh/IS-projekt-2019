48daa0656eae5bc436036dcd582ebbf8508fcabf
erikrisberg98 <56594372+erikrisberg98@users.noreply.github.com>
2019-12-16 15:33:37 +0100

    Test_Erik
    Jag testar github :)

---
+++ src/isprojekt/WrittenExam.java
@@ -0,0 +1,44 @@
+package isprojekt;
+
+public class WrittenExam {
+	
+	private String examID;
+	private String date; 
+	private String location; 
+	private String time; 
+	private int maxPoints;
+	
+	public String getExamID() {
+		return examID;
+	}
+	public void setExamID(String examID) {
+		this.examID = examID;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getLocation() {
+		return location; 
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getMaxPoints() {
+		return maxPoints;
+	}
+	public void setMaxPoints(int maxPoints) {
+		this.maxPoints = maxPoints;
+	} 
+
+	//tjo hej
+
+}
