551ff3b0b6c8f4a744a74c3340ff6e7a568ac827
solgriprosth <56672703+solgriprosth@users.noreply.github.com>
2019-12-18 15:08:58 +0100

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,39 @@
+package src.isprojekt;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class Controller {
+	
+	StudentRegister students; //Refererar till modellen ("Studentregistret")
+	JFrame frame; //Refererar till det grafiska gränssnittet
+	
+	 public Controller(StudentRegister studentReg, JFrame frame) {
+		 this.students = studentReg;
+		 this.frame = frame;
+		 }
+	 
+	 public void addStudent(String studentID, String sName) { 
+		 Student newstudent = new Student(studentID, sName); 
+		 students.addStudent(newstudent);
+	 }
+	 
+	 public void addStudent(String studentID, String sName, String result, String letterGrade) {
+		 Student newstudent = new Student(studentID, sName);
+		 Result studentResult = new Result(result, letterGrade); 
+		 studentResult.setStudent(newstudent);
+		 students.addStudent(newstudent); }
+	 
+	 public void removeStudent(String studentID) { 
+		 students.removeStudent(studentID);	
+	 }
+	 
+	 public Student findStudent(String studentID) {
+		 return students.findStudent(studentID);
+	 }
+	 
+	 
+	 
+
+}
---
+++ CourseRegister.java
@@ -0,0 +1,44 @@
+package src.isprojekt;
+
+import java.util.ArrayList;
+
+public class CourseRegister {
+	
+	private ArrayList<Course>courseRegisterList = new ArrayList<Course>();
+
+	public ArrayList<Course> getCourseRegisterList() {
+		return courseRegisterList;
+	}
+
+	public void setCourseRegisterList(ArrayList<Course> courseRegisterList) {
+		this.courseRegisterList = courseRegisterList;
+	} 
+	
+	public void addCourse(Course c) {
+		courseRegisterList.add(c);
+	}
+	
+	public Course findCourse(String courseCode) {
+		Course c = null; 
+		for(Course co : courseRegisterList) {
+			if(co.getCourseCode().equals(courseCode)) {
+				c = co;
+			}
+		}
+		return c;
+		
+	}
+
+	public Course updateCourse(Course c, String newcourseCode, String newname, double newcredits) {
+		c.setCourseCode(newcourseCode);
+		c.setName(newname);
+		c.setCredits(newcredits);
+			return c;
+	}
+	
+	public Course removeCourse(String courseCode) {
+		Course c = findCourse(courseCode);
+		courseRegisterList.remove(c);
+		return c; 
+	}
+}
