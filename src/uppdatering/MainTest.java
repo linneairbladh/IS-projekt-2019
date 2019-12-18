21ca307abf8dc444881d6d34721f31f55995b596
solgriprosth <solgriprosth@gmail.com>
2019-12-18 14:27:13 +0100

    h

---
+++ src/uppdatering/MainTest.java
@@ -0,0 +1,5 @@
+package uppdatering;
+
+public class MainTest {
+
+}
---
+++ src/uppdatering/WrittenExamRegister.java
@@ -0,0 +1,13 @@
+package uppdatering;
+
+import java.util.ArrayList;
+
+public class WrittenExamRegister {
+
+private ArrayList <WrittenExam> writtenExamList = new ArrayList <WrittenExam>();
+	
+	public void addWrittenExam(WrittenExam a) {
+		writtenExamList.add(a);
+	}
+
+}
