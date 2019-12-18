3573d948a0332142ab61bd659e9336342b50f435
solgriprosth <solgriprosth@gmail.com>
2019-12-18 13:47:26 +0100

    Update Course.java

--- src/src/isprojekt/Course.java
+++ src/src/isprojekt/Course.java
@@ -1,24 +1,23 @@
-package src.isprojekt;
 
+import src.isprojekt.*;
 import java.util.ArrayList;
 
 public class Course {
 	
-	private String courseCode;
+	private int courseCode;
 	private String name;
-	private double credits;
+	private int credits;
 	private ArrayList <WrittenExam> writtenExamList = new ArrayList <WrittenExam>();
  
-	 public Course(String newcourseCode, String newname, int newcredits) {
-		 this.courseCode = newcourseCode; 
-		 this.name = newname; 
-		 this.credits = newcredits; 
-
+	 public Course(int courseCode, int credits) {
+		 this.courseCode = courseCode; 
+		 this.credits = credits; 
+		// TODO Auto-generated constructor stub
 	}
-	public String getCourseCode() {
+	public int getCourseCode() {
 		return courseCode;
 	 }
-	 public void setCourseCode(String courseCode) {
+	 public void setCourseCode(int courseCode) {
 		this.courseCode = courseCode;
 	 }
 	 public String getName() {
@@ -27,10 +26,10 @@ public class Course {
 	 public void setName(String name) {
 		this.name = name;
 	 }
-	 public double getCredits() {
+	 public int getCredits() {
 		return credits;
 	 }
-	 public void setCredits(double credits) {
+	 public void setCredits(int credits) {
 		this.credits = credits;
 	 }
 	 public ArrayList <WrittenExam> getWrittenExamList() {
@@ -43,20 +42,24 @@ public class Course {
 		 writtenExamList.add(a);
 	 }
 	 public WrittenExam findWrittenExam(String examID) {
-		 WrittenExam a = null; 
-			for (WrittenExam a1: writtenExamList) {
-				if (a1.getExamID().equals(examID)) {
-					a1 = a; 
-
+			for (WrittenExam a: writtenExamList) {
+				if (a.getExamID().equals(examID)) {
+					//osäker på examID
+					return a;
+				} else {
+					return null;
 				}
 			}
-			return a;
-	 }
-		
+			return null;
+		}
 	 
-	 public void removeWrittenExam(String examID) {
-		 WrittenExam a1 = this.findWrittenExam(examID);
-				writtenExamList.remove(a1);
+	 public WrittenExam removeWrittenExam(WrittenExam a) {
+		 WrittenExam a1 = this.findWrittenExam(name);
+		 //osäker på name
+			if(a1!= null) {
+				this.writtenExamList.remove(a1);
 			}
+		return a1;
 	}
+	 }
 	 
